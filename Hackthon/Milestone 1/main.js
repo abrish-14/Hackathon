4abe0043ef58c8131bb1951e24d41adb867e178d
ABRISH RATHOOR <159419534+abrish-14@users.noreply.github.com>
2024-09-23 06:37:01 +0500

    push milestone 1

--- Hackthon/Milestone 1/main.js
+++ Hackthon/Milestone 1/main.js
@@ -2,7 +2,7 @@ var toggle_Button = document.getElementById("button");
 var skillss = document.getElementById("skills");
 toggle_Button.addEventListener("click", function () {
     if (skillss.style.display === "none") {
-        skillss.style.display = " block";
+        skillss.style.display = " block";  
     }
     else {
         skillss.style.display = "none";
